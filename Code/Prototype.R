00ef538afe8522d14f9edd4e316fe5c21a9fda9e
Will Beasley <wibeasley@hotmail.com>
2014-02-01 23:00:06 -0600

    Added R file protoype

---
+++ Code/Prototype.R
@@ -0,0 +1,25 @@
+#These first few lines run only when the file is run in RStudio, !!NOT when an Rmd/Rnw file calls it!!
+rm(list=ls(all=TRUE))  #Clear the variables from previous runs.
+
+############################
+## @knitr LoadPackages
+require(RODBC)
+require(plyr)
+
+############################
+## @knitr DeclareGlobals
+
+############################
+## @knitr LoadData
+
+############################
+## @knitr TweakData
+
+############################
+## @knitr AnalysisChunk01
+
+############################
+## @knitr AnalysisChunk02
+
+############################
+## @knitr AnalysisChunk03
---
+++ Documentation/Readme.md
@@ -0,0 +1,3 @@
+# Documentation Directory
+
+In this directory, include documentation about the datasets, such as codebooks.  Make sure the documentation doesn't include passwords, PHI, or anything else that should remain private.  Even if the repository is private, we do NOT treat GitHub as HIPAA compliant (this is just our instinct, and not the official advice from any lawyer).

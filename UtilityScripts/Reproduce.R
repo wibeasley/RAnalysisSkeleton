5ee30067ea1df6ef7f6217a015844ea73f504d13
Will Beasley <wibeasley@hotmail.com>
2014-02-12 17:31:28 -0600

    Adding paths of intermediate files to `Reproduce.R`

--- UtilityScripts/Reproduce.R
+++ UtilityScripts/Reproduce.R
@@ -33,47 +33,64 @@ base::require(markdown)
 base::require(testit)
 
 
-#########################################################################################################
-####
-#### The following example comes from https://github.com/wibeasley/Wats.  Rename the paths appropriately.
-####
-
-
+# #########################################################################################################
+# ####
+# #### The following example comes from https://github.com/wibeasley/Wats.  Rename the paths appropriately.
+# ####
+# 
+# 
 # ###################################
-# # Verify the necessary path can be found.
+# # Declare the paths of the necessary files.
+# 
+# # The raw/input data files:
 # pathCensus199x <- base::paste0("./Datasets/CensusIntercensal/STCH-icen199", 0:9, ".txt")
 # pathCensus200x <- "./Datasets/CensusIntercensal/CO-EST00INT-AGESEX-5YR.csv"
 # pathCountyFips <- "./Datasets/CountyFipsCode.csv"
 # 
+# # The derived/intermediate data files (which are produced by the repository's code files):
+# pathCensusYearly <- "./Datasets/CensusIntercensal/CensusCountyYear.csv"
+# pathCensusMonthly <- "./Datasets/CensusIntercensal/CensusCountyMonth.csv"
+# pathDataForAnalaysis2005 <- "./Datasets/CountyMonthBirthRate2005Version.csv"
+# pathDataForAnalaysis2014 <- "./Datasets/CountyMonthBirthRate2014Version.csv"
+# 
+# # Code Files:
 # pathManipulateCensus <- "./UtilityScripts/IsolateCensusPopsForGfr.R"
 # pathCalculateGfr <- "./UtilityScripts/CalculateGfr.R"
+# 
+# #Report Files:
 # pathsReports <- base::file.path("./vignettes", c("MbrFigures.Rmd", "OkFertilityWithIntercensalEstimates.Rmd"))
 # 
-# # Assert that the working directory has been set properly initial datasets can be found.  The 
+# ###################################
+# # Verify the necessary path can be found.
+# 
+# # The raw/input data files:
 # testit::assert("The 10 census files from 199x should exist.", base::file.exists(pathCensus199x))
 # testit::assert("The 200x census file should exist.", base::file.exists(pathCensus200x))
 # testit::assert("The county FIPS values should exist.", base::file.exists(pathCountyFips))
 # 
+# # Code Files:
 # testit::assert("The file that restructures the census data should exist.", base::file.exists(pathManipulateCensus))
 # testit::assert("The file that calculates the GFR should exist.", base::file.exists(pathCalculateGfr))
+# 
+# #Report Files:
 # testit::assert("The knitr Rmd files should exist.", base::file.exists(pathsReports))
 # 
 # ###################################
 # # Run the files that manipulate and analyze.
 # 
 # # Execute code that restructures the Census data
-# base::source(pathManipulateCensus, local=new.env())
+# base::source(pathManipulateCensus, local=base::new.env())
 # 
 # # Assert that the intermediate files exist (the two files produced by `IsolateCensusPopsForGfr.R`)
-# testit::assert("The yearly records should exist.", base::file.exists("./Datasets/CensusIntercensal/CensusCountyYear.csv"))
-# testit::assert("The monthly records should exist.", base::file.exists("./Datasets/CensusIntercensal/CensusCountyMonth.csv"))
+# testit::assert("The yearly records should exist.", base::file.exists(pathCensusYearly))
+# testit::assert("The monthly records should exist.", base::file.exists(pathCensusMonthly))
 # 
 # #Execute code that combines the census and birth count data.
-# base::source(pathCalculateGfr, local=new.env())
+# base::source(pathCalculateGfr, local=base::new.env())
 # 
 # # Verify that the two human readable datasets are present.
-# testit::assert("The CSV for the 2005 Version should exist.", base::file.exists("./Datasets/CountyMonthBirthRate2005Version.csv"))
-# testit::assert("The CSV for the 2014 Version should exist.", base::file.exists("./Datasets/CountyMonthBirthRate2014Version.csv"))
+# testit::assert("The CSV for the 2005 Version should exist.", base::file.exists(pathDataForAnalaysis2005))
+# testit::assert("The CSV for the 2014 Version should exist.", base::file.exists(pathDataForAnalaysis2014))
 # 
 # ###################################
 # # Build the reports

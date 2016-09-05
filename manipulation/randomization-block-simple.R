e4b5ea021ec1305f101c1663b158187c59a07620
Will Beasley <wibeasley@hotmail.com>
2016-09-05 11:37:41 -0500

    Simple block randomization
    @DavidBard, here is the randomization that we can generalize later.

---
+++ manipulation/randomization-block-simple.R
@@ -0,0 +1,90 @@
+rm(list=ls(all=TRUE))  #Clear the variables from previous runs.
+
+# ---- load-sources ------------------------------------------------------------
+# Call `base::source()` on any repo file that defines functions needed below.  Ideally, no real operations are performed.
+
+
+# ---- load-packages -----------------------------------------------------------
+# Attach these packages so their functions don't need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
+library(magrittr, quietly=TRUE)
+
+# Verify these packages are available on the machine, but their functions need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
+requireNamespace("readr")
+requireNamespace("tibble")
+requireNamespace("tidyr")
+requireNamespace("dplyr") #Avoid attaching dplyr, b/c its function names conflict with a lot of packages (esp base, stats, and plyr).
+requireNamespace("testit") #For asserting conditions meet expected patterns.
+
+
+# ---- declare-globals ---------------------------------------------------------
+set.seed(94)                          # Set seed in care parts need to be modified later.
+block_count                   <- 50L  # Some arbitrarily large number
+block_size                    <- 1L   # Number of assignment cycles per block.
+assignment_possible           <- c("tx", "control")
+assignment_possible_block     <- rep(assignment_possible, each=block_size)
+
+path_out                      <- "data-phi-free/derived/randomized-block-simple.csv"
+
+
+# ---- load-data ---------------------------------------------------------------
+
+# ---- tweak-data --------------------------------------------------------------
+
+
+# ---- assign ------------------------------------------------------------------
+ds <- tibble::tibble(
+  assignment_id         = seq_len(block_count * block_size * length(assignment_possible)),
+  block_id              = rep(seq_len(block_count), each=length(assignment_possible))
+)
+
+ds <- ds %>%
+  dplyr::group_by(block_id) %>%
+  dplyr::mutate(
+    condition           = sample(assignment_possible_block, replace=FALSE),
+    client_id           = '-to be assigned-',
+    year_assigned       = 2016L,
+    month_assigned      = 9L,
+    day_assigned        = '-enter today-'
+  ) %>%
+  dplyr::ungroup()
+
+
+# ---- verify-values -----------------------------------------------------------
+# Sniff out problems
+testit::assert("The `assignment_id` must be nonmissing & positive.", all(!is.na(ds$assignment_id) & (ds$assignment_id>=1)))
+testit::assert("The `block_id` must be nonmissing & positive.", all(!is.na(ds$block_id) & (ds$block_id>=1)))
+testit::assert("The `condition` value must be nonmissing.", all(!is.na(ds$condition)))
+
+testit::assert("The block_id-condition combination should be unique.", all(!duplicated(paste(ds$block_id, ds$condition))))
+
+
+# ---- specify-columns-to-upload -----------------------------------------------
+# dput(colnames(ds)) # Print colnames for line below.
+columns_to_write <- c(
+  "assignment_id", "block_id", "condition", "client_id",
+  "year_assigned", "month_assigned", "day_assigned"
+)
+ds_slim <- ds[, columns_to_write]
+ds_slim
+
+
+# # ---- upload-to-db ------------------------------------------------------------
+# (startTime <- Sys.time())
+# dbTable <- "Osdh.tblC1TEMonth"
+# channel <- RODBC::odbcConnect("te-example") #getSqlTypeInfo("Microsoft SQL Server") #;odbcGetInfo(channel)
+#
+# columnInfo <- RODBC::sqlColumns(channel, dbTable)
+# varTypes <- as.character(columnInfo$TYPE_NAME)
+# names(varTypes) <- as.character(columnInfo$COLUMN_NAME)  #varTypes
+#
+# RODBC::sqlClear(channel, dbTable)
+# RODBC::sqlSave(channel, ds_slim, dbTable, append=TRUE, rownames=FALSE, fast=TRUE, varTypes=varTypes)
+# RODBC::odbcClose(channel)
+# rm(columnInfo, channel, columns_to_write, dbTable, varTypes)
+# (elapsedDuration <-  Sys.time() - startTime) #21.4032 secs 2015-10-31
+
+
+# ---- save-to-disk ------------------------------------------------------------
+readr::write_csv(ds, path_out)
+
+# Consider writing to sqlite (with RSQLite) if there's no PHI, or a central database if there is PHI.

3bf8f87bbac8612733dd9f58368aef91c867e9d0
Will Beasley <wibeasley@hotmail.com>
2020-10-31 15:39:17 -0500

    write to sqlite
    ref #24

--- manipulation/sandbox-dfply-1.py
+++ manipulation/sandbox-dfply-1.py
@@ -4,14 +4,15 @@
 from dfply import *
 import pandas as pd
 import yaml   as y
+import sqlite3
 
 # ---- declare-globals ---------------------------------------------------------
 
 with open(r'config.yml', 'r') as f:
-    config = y.safe_load(f.read().replace('!expr', ''))
+    cfg = y.safe_load(f.read().replace('!expr', ''))
 
 # ---- load-data ---------------------------------------------------------------
-ds = pd.read_csv(config['default']['path_subject_1_raw'])
+ds = pd.read_csv(cfg['default']['path_subject_1_raw'])
 # ds = pd.read_csv("data-public/raw/subject-1.csv")
 
 # ---- tweak-data --------------------------------------------------------------
@@ -28,3 +29,24 @@ ds = (
 )
 
 print(ds.head(3))
+
+# ---- save-to-db --------------------------------------------------------------
+cnn = sqlite3.connect(cfg['default']['path_database'])
+cnn.execute(
+    "\
+        DROP TABLE if exists subject_py;\
+    "
+)
+cnn.execute(
+    "\
+        CREATE TABLE `subject_py` (\
+            subject_id      int   primary key,\
+            county_id       int   not null,\
+            gender_id       float not null,\
+            race            float not null,\
+            ethnicity       float not null\
+        )\
+    "
+)
+ds.to_sql("subject_py", cnn, if_exists = "append", index = False)
+cnn.close()

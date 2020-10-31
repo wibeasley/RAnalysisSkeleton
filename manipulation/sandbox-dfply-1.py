cf83f912bafd65e377adb8a2ca50ce4fffd09a11
Will Beasley <wibeasley@hotmail.com>
2020-10-31 14:53:21 -0500

    yaml for python

--- manipulation/sandbox-dfply-1.py
+++ manipulation/sandbox-dfply-1.py
@@ -1,31 +1,34 @@
+# ---- load-sources ------------------------------------------------------------
+
+# ---- load-packages -----------------------------------------------------------
 from dfply import *
 import pandas as pd
+import yaml   as y
 
-flight_data = pd.read_csv('flight.csv')
-flight_data.head()
-(ds2 = flight_data >>
-  select(X.year)
+# ---- declare-globals ---------------------------------------------------------
+
+with open(r'config.yml') as file:
+    config = y.load(file.read().replace('!expr', ''), Loader = y.FullLoader)
+
+# ---- load-data ---------------------------------------------------------------
+ds = pd.read_csv(config['default']['path_subject_1_raw'])
+# ds = pd.read_csv("data-public/raw/subject-1.csv")
+print(ds)
+
+# ---- tweak-data --------------------------------------------------------------
+ds = (
+    ds >>
+    select(
+        X.subject_id,
+        X.county_id,
+        X.gender_id,
+        X.race,
+        X.ethnicity
+    ) >>
+    arrange(X.subject_id)
 )
 
-(ds2 =
-  flight_data >>
- select(X.year, X.month, X.day) >>
- # drop(X.third_col) >>
- head(3))
+print(ds)
 
 
 
-ds3 = (diamonds >>
- group_by(X.cut) >>
- mutate(
-   price_lead = lead(X.price),
-   price_lag  = lag(X.price) * -1
- ) >>
- head(2) >>
- select(
-   X.cut,
-   X.price,
-   X.price_lead,
-   X.price_lag
-  )
-)

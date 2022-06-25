cb13776afb4da49a1ca16b8e89cae7b5eac2ffb8
Will Beasley <wibeasley@hotmail.com>
2022-06-24 19:54:22 -0500

    script of package dx

---
+++ utility/check-package-health.R
@@ -0,0 +1,29 @@
+rm(list = ls(all.names = TRUE))
+
+spelling::spell_check_package()
+# spelling::update_wordlist()
+
+devtools::document()
+devtools::check_man() #Should return NULL
+# devtools::clean_vignettes()
+# devtools::build_vignettes()
+
+checks_to_exclude <- c(
+  "covr",
+  # "cyclocomp"#,
+  "lintr_line_length_linter"
+)
+gp <-
+  goodpractice::all_checks() |>
+  purrr::discard(~(. %in% checks_to_exclude)) |>
+  {
+    \(checks)
+    goodpractice::gp(checks = checks)
+  }()
+goodpractice::results(gp)
+gp
+
+urlchecker::url_check(); urlchecker::url_update()
+
+# devtools::check(force_suggests = FALSE)
+devtools::check(cran = TRUE)
